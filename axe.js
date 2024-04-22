86aacbefac1bbbe3f4d8f189466c29aa0b110380
romarijs94 <romariojdosantos@gmail.com>
2024-04-21 21:14:19 -0300

    busca por arquivo ./

---
+++ axe.js
@@ -0,0 +1,10 @@
+axe
+  .run()
+  .then((results) => {
+    if (results.violations.length) {
+      throw new Error("Accessibility issues found");
+    }
+  })
+  .catch((err) => {
+    console.error("Something bad happened:", err.message);
+  });
